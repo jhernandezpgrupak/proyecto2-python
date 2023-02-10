6fdc9b87b3161a288bf33cf3d0d3a13b706d934e
jhernandezp <jhernandezp@grupak.com.mx>
2023-02-10 11:58:47 -0600

    comment in to settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,6 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
+#HIDE DJANGO SECRET KEY
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
